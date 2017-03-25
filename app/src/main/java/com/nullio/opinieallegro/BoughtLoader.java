bc6fdc20cf2429fed2e16f41c3c3da4b8712d286
Bartosz Lach <barqlast@gmail.com>
2017-03-25 07:14:42 +0100

    Load bought items

--- app/src/main/java/com/nullio/opinieallegro/BoughtLoader.java
+++ app/src/main/java/com/nullio/opinieallegro/BoughtLoader.java
@@ -18,17 +18,15 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class BoughtLoader {
-
     private List<BoughtItem> boughtItems;
     private Context context;
     private String result;
     public static final String BASE_URL = "https://api.natelefon.pl/";
 
-    public BoughtLoader(Context context){
+    public BoughtLoader(Context context) {
         this.context = context;
         boughtItems = new ArrayList<>();
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-
         StrictMode.setThreadPolicy(policy);
         try {
             init();
@@ -37,23 +35,29 @@ public class BoughtLoader {
         }
     }
 
-    private void init() throws Exception{
+    public List<BoughtItem> getBoughtItems() {
+        return boughtItems;
+    }
+
+    private void init() throws Exception {
         AllegroApiInterface mApiService = this.getInterfaceService();
         Call<BoughtResponse> mService = mApiService.getBought("Bearer " + Constants.userToken);
         Response<BoughtResponse> boughtResponse = null;
         boughtResponse = mService.execute();
         try {
-
             List<OfferResponse> offers = boughtResponse.body().getOffers();
-            for (OfferResponse offer : offers){
-                boughtItems.add(new BoughtItem(offer.getId(), offer.getName(), offer.getMain().getMedium()));
+            for (OfferResponse offer : offers) {
+                String picture = "";
+                if (offer.getMain() != null) {
+                    picture = offer.getMain().getMedium();
+                }
+                boughtItems.add(new BoughtItem(offer.getId(), offer.getName(), picture));
                 Log.i("TAG", "id " + offer.getId());
             }
             result = "success";
             //Log.i("TAG", boughtResponse.body().getOffers().get(0).getMain().getMedium());
-        }
-        catch(Exception e){
-            if (boughtResponse.code() == 403){
+        } catch (Exception e) {
+            if (boughtResponse.code() == 403) {
                 Log.i("TAG", "Wymagane ponowne zalogowanie");
                 setLoggedOff();
                 result = "login";
@@ -72,7 +76,7 @@ public class BoughtLoader {
         return mInterfaceService;
     }
 
-    private void setLoggedOff(){
+    private void setLoggedOff() {
         SharedPreferences settings = context.getSharedPreferences("settings", 0);
         SharedPreferences.Editor editor = settings.edit();
         editor.putLong("logged", 0);
